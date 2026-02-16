4f3a75fdc886769b4f3ec4c21bd94eef95983485
Kalyan Rao <kalyanrao135@gmail.com>
2026-02-16 12:14:23 +0530

    ..

--- app.js
+++ app.js
@@ -3,4 +3,16 @@ const app = express();
 app.get("/",(req,res)=>{
     res.send("Hello Server");
 });
+app.get("/info",(req,res)=>{
+    res.send("Request Method:");
+    console.log(req.method);
+    console.log(req.url);
+
+})
+app.get("/greet",(req,res)=>{
+    const name = req.query.name;
+    const roll = req.query.roll;
+    res.send(`Hello ${name}, your roll is ${roll}`);  
+
+})
 app.listen(3000,()=>{})
